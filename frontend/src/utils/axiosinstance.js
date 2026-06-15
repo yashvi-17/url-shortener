049e9fe55f4e499d870fdf2f35ba1d3796563182
Yashvi <yashvivr06@gmail.com>
2026-06-15 10:51:52 +0300

    fixed frontend deployment issue

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL: "https://url-shortener-backend-ykh8.onrender.com",
     timeout:10000,      //10s
     withCredentials:true
 })
